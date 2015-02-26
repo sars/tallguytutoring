cf2a7b6fcfb62736b366062628dbac78cd93afe0
Taras Pochkun <pochkun_taras@mail.ru>
2015-02-26 12:45:36 +0200

    restyle first page and put new text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def width_brogress_bar(referals_cout, max_referals_count)
+    referals_cout >= max_referals_count ? 1120 : 1000 / max_referals_count * referals_cout + 120
+  end
 end
