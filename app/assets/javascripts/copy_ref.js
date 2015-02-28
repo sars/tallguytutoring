1235119cdb08233697c1e2d07fbf92932224e9be
Taras Pochkun <pochkun_taras@mail.ru>
2015-02-28 20:00:33 +0200

    Media changes 3

--- app/assets/javascripts/copy_ref.js
+++ app/assets/javascripts/copy_ref.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $(".copy-link.brandon").zclip({
+    var hasFlash = false;
+    try {
+        hasFlash = Boolean(new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
+    } catch(exception) {
+        hasFlash = ('undefined' != typeof navigator.mimeTypes['application/x-shockwave-flash']);
+    }
+
+    hasFlash && $(".copy-link.brandon").zclip({
         path: '/assets/ZeroClipboard.swf',
         copy: $('.copy-link.brandon').text(),
         afterCopy: function(){
